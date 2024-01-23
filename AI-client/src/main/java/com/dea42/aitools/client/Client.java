4102b3707b74352092f38aebad475fbc02293213
avatar42 <_github@m42.biz>
2024-01-22 22:20:17 -0600

    add markup and crop

--- AI-client/src/main/java/com/dea42/aitools/client/Client.java
+++ AI-client/src/main/java/com/dea42/aitools/client/Client.java
@@ -1,11 +1,17 @@
 package com.dea42.aitools.client;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpStatus;
@@ -19,6 +25,8 @@ import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,17 +41,45 @@ public class Client {
 	private static final Marker CLIENT_MARKER = MarkerFactory.getMarker("CLIENT_MARKER");
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	private String newFolder = "./new";
+	private String markedFolder = "./marked";
+	private String labeledFolder = "./labeled";
+	private String debugFolder = "./debug";
+
+	public String getMarkedFolder() {
+		return markedFolder;
+	}
+
+	public void setMarkedFolder(String markedFolder) {
+		this.markedFolder = markedFolder;
+	}
+
+	public String getLabeledFolder() {
+		return labeledFolder;
+	}
+
+	public void setLabeledFolder(String labeledFolder) {
+		this.labeledFolder = labeledFolder;
+	}
+
+	public String getDebugFolder() {
+		return debugFolder;
+	}
+
+	public void setDebugFolder(String debugFolder) {
+		this.debugFolder = debugFolder;
+	}
+
 	private String host = "http://10.10.2.183:32168";
 
 	public Client(String host, String newFolder) {
 		this.newFolder = newFolder;
 		this.host = host;
 	}
-	
+
 	public Client() {
-			ResourceBundle bundle = ResourceBundle.getBundle("client");
-			this.host = bundle.getString("host");
-			this.newFolder = bundle.getString("folder.new");
+		ResourceBundle bundle = ResourceBundle.getBundle("client");
+		this.host = bundle.getString("host");
+		this.newFolder = bundle.getString("folder.new");
 	}
 
 	private String doPost(HttpEntity params, String url) throws IOException {
@@ -81,6 +117,69 @@ public class Client {
 
 	}
 
+	/*
+	 * {"confidence":0.49429088830947876,"label":"cat_grey","x_min":383,"y_min":294,
+	 * "x_max":512,"y_max":358}
+	 */
+	public BufferedImage markImg(String model, BufferedImage img, JSONObject jobj, Color c) {
+		// Obtain the Graphics2D context associated with the BufferedImage.
+		Graphics2D g = img.createGraphics();
+
+		// Draw on the BufferedImage via the graphics context.
+		int x = jobj.optInt("x_min");
+		int y = jobj.optInt("y_min");
+		int width = jobj.optInt("x_max") - x;
+		int height = jobj.optInt("y_max") - y;
+
+		g.drawRect(x, y, width, height);
+		g.drawString(model+"_"+jobj.getString("label"), x, y);
+		// Clean up -- dispose the graphics context that was created.
+		g.dispose();
+
+		return img;
+	}
+
+	/**
+	 * Get the detections of an images as individual files as
+	 * debugFolder/class/idx.fileName<br>
+	 * Mainly used for easily checking
+	 * 
+	 * @param file to read from
+	 * @param ja   JSONArray of detections
+	 * @return number of detections that match the class the source file came from.
+	 * @throws JSONException
+	 * @throws IOException
+	 */
+	public int cropImgs(File file, JSONArray ja) throws JSONException, IOException {
+		BufferedImage img = ImageIO.read(file);
+		String cls = file.getParentFile().getName();
+		int matches = 0;
+		for (int i = 0; i < ja.length(); i++) {
+			JSONObject jobj = ja.getJSONObject(i);
+			int x = jobj.optInt("x_min");
+			int y = jobj.optInt("y_min");
+			int width = jobj.optInt("x_max") - x;
+			int height = jobj.optInt("y_max") - y;
+			String dcls = jobj.getString("label");
+			if (cls.equals(dcls))
+				matches++;
+			BufferedImage simg = img.getSubimage(x, y, width, height);
+			writeImg(getDebugFolder(), dcls + "/" + i + "." + file.getName(), simg);
+		}
+		return matches;
+	}
+
+	public void writeImg(String folder, String path, BufferedImage bufferedImage) throws IOException {
+		RenderedImage rendImage = bufferedImage;
+
+		File file = new File(folder, path);
+		File dir = file.getParentFile();
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+		ImageIO.write(rendImage, "jpg", file);
+	}
+
 	private MultipartEntityBuilder addImage(MultipartEntityBuilder reqEntity, String imageName) throws IOException {
 		File fileToUse = new File(newFolder, imageName);
 		if (fileToUse.exists()) {
@@ -91,6 +190,22 @@ public class Client {
 			return reqEntity;
 		}
 		throw new IOException(fileToUse.getAbsolutePath() + " does not exist");
+	}
+
+	public String getNewFolder() {
+		return newFolder;
+	}
+
+	public void setNewFolder(String newFolder) {
+		this.newFolder = newFolder;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
 	}
 
 	public JSONObject detect(String model, String imagePath, String minConfidence) throws IOException {
--- AI-client/src/test/java/com/dea42/aitools/client/ClientTest.java
+++ AI-client/src/test/java/com/dea42/aitools/client/ClientTest.java
@@ -1,9 +1,16 @@
 package com.dea42.aitools.client;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.Test;
 
@@ -11,7 +18,9 @@ import org.junit.Test;
  * Unit test for client.
  */
 public class ClientTest {
-	Client obj = new Client();
+	private Client obj = new Client();
+	private String testFile1 = "animal/cat/cat_black/PTZ510.20230510_050201.9815571.3.jpg";
+	private String testFile2 = "animal/raccoon/HV420.20230722_214738243.2756.jpg";
 
 	/**
 	 * Basic Detect Test
@@ -20,7 +29,7 @@ public class ClientTest {
 	 */
 	@Test
 	public void detectionTest() throws IOException {
-		JSONObject resp = obj.detect("detection", "animal/cat/cat_black/PTZ510.20230510_050201.9815571.3.jpg", "0.55");
+		JSONObject resp = obj.detect("detection", testFile1, "0.55");
 		// {"message":"Found cat, dog,
 		// bowl","count":3,"predictions":[{"confidence":0.4594840705394745,"label":"cat","x_min":379,"y_min":295,"x_max":514,"y_max":357},{"confidence":0.5552480220794678,"label":"dog","x_min":380,"y_min":295,"x_max":511,"y_max":357},{"confidence":0.7309744358062744,"label":"bowl","x_min":321,"y_min":391,"x_max":361,"y_max":423}],"success":true,"processMs":714,"inferenceMs":714,"code":200,"command":"detect","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":732}
 		assertEquals("message wrong", "Found cat, dog, bowl", (String) resp.get("message"));
@@ -33,22 +42,69 @@ public class ClientTest {
 	 */
 	@Test
 	public void RMRRTest() throws IOException {
-		JSONObject resp = obj.detect("custom/RMRR", "animal/cat/cat_black/PTZ510.20230510_050201.9815571.3.jpg",
-				"0.45");
-		// {"message":"Found cat_grey","count":1,"predictions":[{"confidence":0.49429088830947876,"label":"cat_grey","x_min":383,"y_min":294,"x_max":512,"y_max":358}],"success":true,"processMs":730,"inferenceMs":730,"code":200,"command":"custom","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":750}
+		JSONObject resp = obj.detect("custom/RMRR", testFile1, "0.45");
+		// {"message":"Found
+		// cat_grey","count":1,"predictions":[{"confidence":0.49429088830947876,"label":"cat_grey","x_min":383,"y_min":294,"x_max":512,"y_max":358}],"success":true,"processMs":730,"inferenceMs":730,"code":200,"command":"custom","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":750}
 		assertEquals("message wrong", "Found cat_grey", (String) resp.get("message"));
 	}
 
 	/**
-	 * Basic RMRR Test
+	 * Basic dark Test
 	 * 
 	 * @throws IOException
 	 */
 	@Test
 	public void darkTest() throws IOException {
-		JSONObject resp = obj.detect("custom/dark", "animal/cat/cat_black/PTZ510.20230510_050201.9815571.3.jpg",
-				"0.74");
-		// {"message":"Found Cat","count":1,"predictions":[{"confidence":0.7439730763435364,"label":"Cat","x_min":376,"y_min":285,"x_max":520,"y_max":361}],"success":true,"processMs":3297,"inferenceMs":2380,"code":200,"command":"custom","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":3318}
+		JSONObject resp = obj.detect("custom/dark", testFile1, "0.74");
+		// {"message":"Found
+		// Cat","count":1,"predictions":[{"confidence":0.7439730763435364,"label":"Cat","x_min":376,"y_min":285,"x_max":520,"y_max":361}],"success":true,"processMs":3297,"inferenceMs":2380,"code":200,"command":"custom","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":3318}
 		assertEquals("message wrong", "Found Cat", (String) resp.get("message"));
 	}
+
+	@Test
+	public void RMRRmarkupTest() throws IOException {
+		JSONObject resp = obj.detect("custom/RMRR", testFile1, "0.45");
+		// {"message":"Found
+		// cat_grey","count":1,"predictions":[{"confidence":0.49429088830947876,"label":"cat_grey","x_min":383,"y_min":294,"x_max":512,"y_max":358}],"success":true,"processMs":730,"inferenceMs":730,"code":200,"command":"custom","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":750}
+		assertEquals("message wrong", "Found cat_grey", (String) resp.get("message"));
+
+		BufferedImage img = ImageIO.read(new File(obj.getNewFolder(), testFile1));
+		img = obj.markImg(null, img, ((JSONArray) resp.get("predictions")).getJSONObject(0), Color.BLUE);
+		obj.writeImg(obj.getMarkedFolder(), testFile1, img);
+	}
+
+	@Test
+	public void RMRRcropTest() throws IOException {
+		JSONObject resp = obj.detect("custom/RMRR", testFile1, "0.45");
+		// {"message":"Found
+		// cat_grey","count":1,"predictions":[{"confidence":0.49429088830947876,"label":"cat_grey","x_min":383,"y_min":294,"x_max":512,"y_max":358}],"success":true,"processMs":730,"inferenceMs":730,"code":200,"command":"custom","moduleId":"ObjectDetectionYolo","executionProvider":"CPU","canUseGPU":false,"analysisRoundTripMs":750}
+		assertEquals("message wrong", "Found cat_grey", (String) resp.get("message"));
+
+		int matches = obj.cropImgs(new File(obj.getNewFolder(), testFile1), ((JSONArray) resp.get("predictions")));
+		assertEquals("matches", 0, matches);
+	}
+
+	@Test
+	public void complexMarkupTest() throws IOException {
+		BufferedImage img = ImageIO.read(new File(obj.getNewFolder(), testFile2));
+
+		JSONObject resp = obj.detect("custom/RMRR", testFile2, "0.45");
+		assertTrue("Class wrong", resp.getString("message").contains("raccoon"));
+		JSONArray predictions = resp.getJSONArray("predictions");
+		assertEquals("predictions", 7,predictions.length());
+		for (int i=0; i<predictions.length();i++) {
+			img = obj.markImg("RMRR", img, predictions.getJSONObject(i), Color.BLUE);
+		}
+//		obj.writeImg(obj.getMarkedFolder(), testFile1, img);
+
+		resp = obj.detect("detection", testFile2, "0.45");
+		assertTrue("Class wrong", resp.getString("message").contains("cat"));
+		predictions = resp.getJSONArray("predictions");
+		assertEquals("predictions", 5,predictions.length());
+		for (int i=0; i<predictions.length();i++) {
+			img = obj.markImg("detection", img, predictions.getJSONObject(i), Color.BLUE);
+		}
+		obj.writeImg(obj.getMarkedFolder(), testFile2, img);
+	}
+
 }
